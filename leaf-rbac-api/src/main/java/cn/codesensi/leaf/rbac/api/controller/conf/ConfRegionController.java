dde752af542125ab659aede20e6aaebd0d8e4e9c
codesensi <codesensi@aliyun.com>
2026-07-13 23:27:56 +0800

    feat(conf): 为新政区划配置添加操作日志记录功能
    - 引入 OperateType 枚举类型用于操作类型标识
    - 添加 LogOperate 注解到导入民政部行政区划接口
    - 配置操作模块名称为"新政区划配置"
    - 设置操作类型为 INSERT 类型
    - 定义操作描述为"从民政部导入行政区划"

--- leaf-rbac-api/src/main/java/cn/codesensi/leaf/rbac/api/controller/conf/ConfRegionController.java
+++ leaf-rbac-api/src/main/java/cn/codesensi/leaf/rbac/api/controller/conf/ConfRegionController.java
@@ -1,6 +1,8 @@
 package cn.codesensi.leaf.rbac.api.controller.conf;
 
+import cn.codesensi.leaf.rbac.common.enums.OperateType;
 import cn.codesensi.leaf.rbac.framework.annotation.ApiResponseBody;
+import cn.codesensi.leaf.rbac.framework.annotation.LogOperate;
 import cn.codesensi.leaf.rbac.system.service.ConfRegionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -29,6 +31,7 @@ public class ConfRegionController {
      *
      * @return 导入数量
      */
+    @LogOperate(module = "新政区划配置", type = OperateType.INSERT, desc = "从民政部导入行政区划")
     @Operation(summary = "从民政部导入行政区划", description = "从民政部导入行政区划")
     @PostMapping("/importFromMCA")
     public Integer importFromMCA() {
