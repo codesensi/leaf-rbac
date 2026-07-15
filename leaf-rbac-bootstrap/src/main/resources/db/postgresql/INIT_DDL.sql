6936db0522ab8f7f82166bc7cae9bb15e59670e8
codesensi <codesensi@aliyun.com>
2026-07-15 23:22:14 +0800

    style(database): 格式化 PostgreSQL 数据库初始化脚本中的注释
    - 统一 COMMENT 语句格式，每条注释独立成行
    - 修正 sys_user 表所有字段注释格式
    - 修正 sys_role 表所有字段注释格式
    - 修正 sys_user_role 表所有字段注释格式
    - 修正 sys_menu 表所有字段注释格式
    - 修正 sys_role_menu 表所有字段注释格式
    - 修正 log_login 表所有字段注释格式
    - 修正 log_operate 表所有字段注释格式
    - 修正 conf_region 表所有字段注释格式
    - 调整 log_operate 表 user_id 字段对齐格式

--- leaf-rbac-bootstrap/src/main/resources/db/postgresql/INIT_DDL.sql
+++ leaf-rbac-bootstrap/src/main/resources/db/postgresql/INIT_DDL.sql
@@ -28,25 +28,44 @@ CREATE TABLE sys_user
 CREATE INDEX idx_u_username ON sys_user (username);
 CREATE INDEX idx_u_status ON sys_user (status);
 
-COMMENT ON TABLE sys_user IS '用户信息表';
-COMMENT ON COLUMN sys_user.id IS '用户ID';
-COMMENT ON COLUMN sys_user.username IS '用户名称';
-COMMENT ON COLUMN sys_user.password IS '用户密码';
-COMMENT ON COLUMN sys_user.nickname IS '用户昵称';
-COMMENT ON COLUMN sys_user.id_no IS '用户身份证号码';
-COMMENT ON COLUMN sys_user.email IS '用户邮箱';
-COMMENT ON COLUMN sys_user.phone IS '用户手机号码';
-COMMENT ON COLUMN sys_user.gender IS '用户性别:0-保密,1-男,2-女';
-COMMENT ON COLUMN sys_user.avatar IS '用户头像地址';
-COMMENT ON COLUMN sys_user.type IS '用户类型:0-系统用户';
-COMMENT ON COLUMN sys_user.status IS '用户状态:0-启用,1-禁用';
-COMMENT ON COLUMN sys_user.remark IS '备注';
-COMMENT ON COLUMN sys_user.sys_flag IS '系统内置标识:0-否,1-是';
-COMMENT ON COLUMN sys_user.creator IS '创建人';
-COMMENT ON COLUMN sys_user.create_time IS '创建时间';
-COMMENT ON COLUMN sys_user.updater IS '更新人';
-COMMENT ON COLUMN sys_user.update_time IS '更新时间';
-COMMENT ON COLUMN sys_user.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
+COMMENT
+ON TABLE sys_user IS '用户信息表';
+COMMENT
+ON COLUMN sys_user.id IS '用户ID';
+COMMENT
+ON COLUMN sys_user.username IS '用户名称';
+COMMENT
+ON COLUMN sys_user.password IS '用户密码';
+COMMENT
+ON COLUMN sys_user.nickname IS '用户昵称';
+COMMENT
+ON COLUMN sys_user.id_no IS '用户身份证号码';
+COMMENT
+ON COLUMN sys_user.email IS '用户邮箱';
+COMMENT
+ON COLUMN sys_user.phone IS '用户手机号码';
+COMMENT
+ON COLUMN sys_user.gender IS '用户性别:0-保密,1-男,2-女';
+COMMENT
+ON COLUMN sys_user.avatar IS '用户头像地址';
+COMMENT
+ON COLUMN sys_user.type IS '用户类型:0-系统用户';
+COMMENT
+ON COLUMN sys_user.status IS '用户状态:0-启用,1-禁用';
+COMMENT
+ON COLUMN sys_user.remark IS '备注';
+COMMENT
+ON COLUMN sys_user.sys_flag IS '系统内置标识:0-否,1-是';
+COMMENT
+ON COLUMN sys_user.creator IS '创建人';
+COMMENT
+ON COLUMN sys_user.create_time IS '创建时间';
+COMMENT
+ON COLUMN sys_user.updater IS '更新人';
+COMMENT
+ON COLUMN sys_user.update_time IS '更新时间';
+COMMENT
+ON COLUMN sys_user.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
 
 
 -- ----------------------------
@@ -74,20 +93,34 @@ CREATE TABLE sys_role
 CREATE INDEX idx_r_code ON sys_role (code);
 CREATE INDEX idx_r_status ON sys_role (status);
 
-COMMENT ON TABLE sys_role IS '角色信息表';
-COMMENT ON COLUMN sys_role.id IS '角色ID';
-COMMENT ON COLUMN sys_role.name IS '角色名称';
-COMMENT ON COLUMN sys_role.code IS '角色编码';
-COMMENT ON COLUMN sys_role.pid IS '父角色ID';
-COMMENT ON COLUMN sys_role.sort IS '角色排序';
-COMMENT ON COLUMN sys_role.status IS '角色状态:0-启用,1-禁用';
-COMMENT ON COLUMN sys_role.remark IS '备注';
-COMMENT ON COLUMN sys_role.sys_flag IS '系统内置标识:0-自定义,1-内置';
-COMMENT ON COLUMN sys_role.creator IS '创建人';
-COMMENT ON COLUMN sys_role.create_time IS '创建时间';
-COMMENT ON COLUMN sys_role.updater IS '更新人';
-COMMENT ON COLUMN sys_role.update_time IS '更新时间';
-COMMENT ON COLUMN sys_role.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
+COMMENT
+ON TABLE sys_role IS '角色信息表';
+COMMENT
+ON COLUMN sys_role.id IS '角色ID';
+COMMENT
+ON COLUMN sys_role.name IS '角色名称';
+COMMENT
+ON COLUMN sys_role.code IS '角色编码';
+COMMENT
+ON COLUMN sys_role.pid IS '父角色ID';
+COMMENT
+ON COLUMN sys_role.sort IS '角色排序';
+COMMENT
+ON COLUMN sys_role.status IS '角色状态:0-启用,1-禁用';
+COMMENT
+ON COLUMN sys_role.remark IS '备注';
+COMMENT
+ON COLUMN sys_role.sys_flag IS '系统内置标识:0-自定义,1-内置';
+COMMENT
+ON COLUMN sys_role.creator IS '创建人';
+COMMENT
+ON COLUMN sys_role.create_time IS '创建时间';
+COMMENT
+ON COLUMN sys_role.updater IS '更新人';
+COMMENT
+ON COLUMN sys_role.update_time IS '更新时间';
+COMMENT
+ON COLUMN sys_role.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
 
 
 -- ----------------------------
@@ -110,15 +143,24 @@ CREATE TABLE sys_user_role
 CREATE INDEX idx_ur_user_id ON sys_user_role (user_id);
 CREATE INDEX idx_ur_role_id ON sys_user_role (role_id);
 
-COMMENT ON TABLE sys_user_role IS '用户角色关联表';
-COMMENT ON COLUMN sys_user_role.id IS '主键ID';
-COMMENT ON COLUMN sys_user_role.user_id IS '用户ID';
-COMMENT ON COLUMN sys_user_role.role_id IS '角色ID';
-COMMENT ON COLUMN sys_user_role.creator IS '创建人';
-COMMENT ON COLUMN sys_user_role.create_time IS '创建时间';
-COMMENT ON COLUMN sys_user_role.updater IS '更新人';
-COMMENT ON COLUMN sys_user_role.update_time IS '更新时间';
-COMMENT ON COLUMN sys_user_role.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
+COMMENT
+ON TABLE sys_user_role IS '用户角色关联表';
+COMMENT
+ON COLUMN sys_user_role.id IS '主键ID';
+COMMENT
+ON COLUMN sys_user_role.user_id IS '用户ID';
+COMMENT
+ON COLUMN sys_user_role.role_id IS '角色ID';
+COMMENT
+ON COLUMN sys_user_role.creator IS '创建人';
+COMMENT
+ON COLUMN sys_user_role.create_time IS '创建时间';
+COMMENT
+ON COLUMN sys_user_role.updater IS '更新人';
+COMMENT
+ON COLUMN sys_user_role.update_time IS '更新时间';
+COMMENT
+ON COLUMN sys_user_role.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
 
 
 -- ----------------------------
@@ -156,31 +198,56 @@ CREATE TABLE sys_menu
 
 CREATE INDEX idx_m_status ON sys_menu (status);
 
-COMMENT ON TABLE sys_menu IS '路由菜单表';
-COMMENT ON COLUMN sys_menu.id IS '路由菜单ID';
-COMMENT ON COLUMN sys_menu.pid IS '父级路由菜单ID';
-COMMENT ON COLUMN sys_menu.name IS '路由名称(外链地址)';
-COMMENT ON COLUMN sys_menu.path IS '路由路径';
-COMMENT ON COLUMN sys_menu.param IS '路由参数';
-COMMENT ON COLUMN sys_menu.component IS '组件路径';
-COMMENT ON COLUMN sys_menu.title IS '菜单名称';
-COMMENT ON COLUMN sys_menu.type IS '菜单类型:1-目录,2-菜单,3-按钮';
-COMMENT ON COLUMN sys_menu.sort IS '菜单排序';
-COMMENT ON COLUMN sys_menu.icon IS '菜单图标';
-COMMENT ON COLUMN sys_menu.perms IS '权限编码';
-COMMENT ON COLUMN sys_menu.is_link IS '是否外链:0-否,1-是';
-COMMENT ON COLUMN sys_menu.is_frame IS '是否内嵌iframe:0-否,1-是';
-COMMENT ON COLUMN sys_menu.frame_src IS '内嵌iframe地址';
-COMMENT ON COLUMN sys_menu.is_show IS '是否显示:0-否,1-是';
-COMMENT ON COLUMN sys_menu.is_show_parent IS '是否显示父级菜单:0-否,1-是';
-COMMENT ON COLUMN sys_menu.status IS '菜单状态:0-启用,1-禁用';
-COMMENT ON COLUMN sys_menu.remark IS '备注';
-COMMENT ON COLUMN sys_menu.sys_flag IS '系统内置标识:0-自定义,1-内置';
-COMMENT ON COLUMN sys_menu.creator IS '创建人';
-COMMENT ON COLUMN sys_menu.create_time IS '创建时间';
-COMMENT ON COLUMN sys_menu.updater IS '更新人';
-COMMENT ON COLUMN sys_menu.update_time IS '更新时间';
-COMMENT ON COLUMN sys_menu.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
+COMMENT
+ON TABLE sys_menu IS '路由菜单表';
+COMMENT
+ON COLUMN sys_menu.id IS '路由菜单ID';
+COMMENT
+ON COLUMN sys_menu.pid IS '父级路由菜单ID';
+COMMENT
+ON COLUMN sys_menu.name IS '路由名称(外链地址)';
+COMMENT
+ON COLUMN sys_menu.path IS '路由路径';
+COMMENT
+ON COLUMN sys_menu.param IS '路由参数';
+COMMENT
+ON COLUMN sys_menu.component IS '组件路径';
+COMMENT
+ON COLUMN sys_menu.title IS '菜单名称';
+COMMENT
+ON COLUMN sys_menu.type IS '菜单类型:1-目录,2-菜单,3-按钮';
+COMMENT
+ON COLUMN sys_menu.sort IS '菜单排序';
+COMMENT
+ON COLUMN sys_menu.icon IS '菜单图标';
+COMMENT
+ON COLUMN sys_menu.perms IS '权限编码';
+COMMENT
+ON COLUMN sys_menu.is_link IS '是否外链:0-否,1-是';
+COMMENT
+ON COLUMN sys_menu.is_frame IS '是否内嵌iframe:0-否,1-是';
+COMMENT
+ON COLUMN sys_menu.frame_src IS '内嵌iframe地址';
+COMMENT
+ON COLUMN sys_menu.is_show IS '是否显示:0-否,1-是';
+COMMENT
+ON COLUMN sys_menu.is_show_parent IS '是否显示父级菜单:0-否,1-是';
+COMMENT
+ON COLUMN sys_menu.status IS '菜单状态:0-启用,1-禁用';
+COMMENT
+ON COLUMN sys_menu.remark IS '备注';
+COMMENT
+ON COLUMN sys_menu.sys_flag IS '系统内置标识:0-自定义,1-内置';
+COMMENT
+ON COLUMN sys_menu.creator IS '创建人';
+COMMENT
+ON COLUMN sys_menu.create_time IS '创建时间';
+COMMENT
+ON COLUMN sys_menu.updater IS '更新人';
+COMMENT
+ON COLUMN sys_menu.update_time IS '更新时间';
+COMMENT
+ON COLUMN sys_menu.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
 
 
 -- ----------------------------
@@ -203,15 +270,24 @@ CREATE TABLE sys_role_menu
 CREATE INDEX idx_rm_role_id ON sys_role_menu (role_id);
 CREATE INDEX idx_rm_menu_id ON sys_role_menu (menu_id);
 
-COMMENT ON TABLE sys_role_menu IS '角色菜单关联表';
-COMMENT ON COLUMN sys_role_menu.id IS '主键ID';
-COMMENT ON COLUMN sys_role_menu.role_id IS '角色ID';
-COMMENT ON COLUMN sys_role_menu.menu_id IS '菜单ID';
-COMMENT ON COLUMN sys_role_menu.creator IS '创建人';
-COMMENT ON COLUMN sys_role_menu.create_time IS '创建时间';
-COMMENT ON COLUMN sys_role_menu.updater IS '更新人';
-COMMENT ON COLUMN sys_role_menu.update_time IS '更新时间';
-COMMENT ON COLUMN sys_role_menu.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
+COMMENT
+ON TABLE sys_role_menu IS '角色菜单关联表';
+COMMENT
+ON COLUMN sys_role_menu.id IS '主键ID';
+COMMENT
+ON COLUMN sys_role_menu.role_id IS '角色ID';
+COMMENT
+ON COLUMN sys_role_menu.menu_id IS '菜单ID';
+COMMENT
+ON COLUMN sys_role_menu.creator IS '创建人';
+COMMENT
+ON COLUMN sys_role_menu.create_time IS '创建时间';
+COMMENT
+ON COLUMN sys_role_menu.updater IS '更新人';
+COMMENT
+ON COLUMN sys_role_menu.update_time IS '更新时间';
+COMMENT
+ON COLUMN sys_role_menu.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
 
 
 -- Table structure for log_login
@@ -242,27 +318,48 @@ CREATE TABLE log_login
     PRIMARY KEY (id)
 );
 
-COMMENT ON TABLE log_login IS '登录日志表';
-COMMENT ON COLUMN log_login.id IS '日志ID';
-COMMENT ON COLUMN log_login.login_type IS '登录方式:0-未知,1-账号密码,2-手机号';
-COMMENT ON COLUMN log_login.event_type IS '事件类型:0-未知,1-登录,2-登出';
-COMMENT ON COLUMN log_login.login_key IS '登录标识:账号/手机号';
-COMMENT ON COLUMN log_login.user_id IS '登录人ID';
-COMMENT ON COLUMN log_login.username IS '登录人账号';
-COMMENT ON COLUMN log_login.status IS '登录状态:0-失败,1-成功';
-COMMENT ON COLUMN log_login.error_msg IS '登录失败原因';
-COMMENT ON COLUMN log_login.request_ip IS '请求来源IP地址';
-COMMENT ON COLUMN log_login.request_area IS '登录地区';
-COMMENT ON COLUMN log_login.request_os IS '登录系统';
-COMMENT ON COLUMN log_login.request_device IS '登录设备';
-COMMENT ON COLUMN log_login.request_browser IS '登录浏览器';
-COMMENT ON COLUMN log_login.duration_ms IS '请求执行耗时:单位毫秒';
-COMMENT ON COLUMN log_login.params IS '请求参数';
-COMMENT ON COLUMN log_login.creator IS '创建人';
-COMMENT ON COLUMN log_login.create_time IS '创建时间';
-COMMENT ON COLUMN log_login.updater IS '更新人';
-COMMENT ON COLUMN log_login.update_time IS '更新时间';
-COMMENT ON COLUMN log_login.del_flag IS '是否删除:0-否,1-是';
+COMMENT
+ON TABLE log_login IS '登录日志表';
+COMMENT
+ON COLUMN log_login.id IS '日志ID';
+COMMENT
+ON COLUMN log_login.login_type IS '登录方式:0-未知,1-账号密码,2-手机号';
+COMMENT
+ON COLUMN log_login.event_type IS '事件类型:0-未知,1-登录,2-登出';
+COMMENT
+ON COLUMN log_login.login_key IS '登录标识:账号/手机号';
+COMMENT
+ON COLUMN log_login.user_id IS '登录人ID';
+COMMENT
+ON COLUMN log_login.username IS '登录人账号';
+COMMENT
+ON COLUMN log_login.status IS '登录状态:0-失败,1-成功';
+COMMENT
+ON COLUMN log_login.error_msg IS '登录失败原因';
+COMMENT
+ON COLUMN log_login.request_ip IS '请求来源IP地址';
+COMMENT
+ON COLUMN log_login.request_area IS '登录地区';
+COMMENT
+ON COLUMN log_login.request_os IS '登录系统';
+COMMENT
+ON COLUMN log_login.request_device IS '登录设备';
+COMMENT
+ON COLUMN log_login.request_browser IS '登录浏览器';
+COMMENT
+ON COLUMN log_login.duration_ms IS '请求执行耗时:单位毫秒';
+COMMENT
+ON COLUMN log_login.params IS '请求参数';
+COMMENT
+ON COLUMN log_login.creator IS '创建人';
+COMMENT
+ON COLUMN log_login.create_time IS '创建时间';
+COMMENT
+ON COLUMN log_login.updater IS '更新人';
+COMMENT
+ON COLUMN log_login.update_time IS '更新时间';
+COMMENT
+ON COLUMN log_login.del_flag IS '是否删除:0-否,1-是';
 
 
 -- ----------------------------
@@ -274,7 +371,7 @@ CREATE TABLE log_operate
     id              bigint NOT NULL,
     module          varchar(256) NULL DEFAULT NULL,
     type            smallint NULL DEFAULT NULL,
-    user_id        bigint NULL DEFAULT NULL,
+    user_id         bigint NULL DEFAULT NULL,
     descr           varchar(256) NULL DEFAULT NULL,
     status          smallint NULL DEFAULT NULL,
     error_msg       text NULL DEFAULT NULL,
@@ -296,29 +393,52 @@ CREATE TABLE log_operate
     PRIMARY KEY (id)
 );
 
-COMMENT ON TABLE log_operate IS '操作日志表';
-COMMENT ON COLUMN log_operate.id IS '日志ID';
-COMMENT ON COLUMN log_operate.module IS '操作所属模块';
-COMMENT ON COLUMN log_operate.type IS '操作类型:0-未知,1-新增,2-更新,3-查询,4-删除';
-COMMENT ON COLUMN log_operate.user_id IS '操作人ID';
-COMMENT ON COLUMN log_operate.descr IS '操作描述';
-COMMENT ON COLUMN log_operate.status IS '操作状态:0-失败,1-成功';
-COMMENT ON COLUMN log_operate.error_msg IS '错误信息';
-COMMENT ON COLUMN log_operate.request_ip IS '请求IP';
-COMMENT ON COLUMN log_operate.request_url IS '请求的URL地址';
-COMMENT ON COLUMN log_operate.request_area IS '请求地区';
-COMMENT ON COLUMN log_operate.request_os IS '请求系统';
-COMMENT ON COLUMN log_operate.request_device IS '请求设备';
-COMMENT ON COLUMN log_operate.request_browser IS '请求浏览器';
-COMMENT ON COLUMN log_operate.method_name IS '被调用方法的全限定名（包名.类名.方法名）';
-COMMENT ON COLUMN log_operate.duration_ms IS '请求执行耗时:单位毫秒';
-COMMENT ON COLUMN log_operate.params IS '请求参数';
-COMMENT ON COLUMN log_operate.result IS '响应结果';
-COMMENT ON COLUMN log_operate.creator IS '创建人';
-COMMENT ON COLUMN log_operate.create_time IS '创建时间';
-COMMENT ON COLUMN log_operate.updater IS '更新人';
-COMMENT ON COLUMN log_operate.update_time IS '更新时间';
-COMMENT ON COLUMN log_operate.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
+COMMENT
+ON TABLE log_operate IS '操作日志表';
+COMMENT
+ON COLUMN log_operate.id IS '日志ID';
+COMMENT
+ON COLUMN log_operate.module IS '操作所属模块';
+COMMENT
+ON COLUMN log_operate.type IS '操作类型:0-未知,1-新增,2-更新,3-查询,4-删除';
+COMMENT
+ON COLUMN log_operate.user_id IS '操作人ID';
+COMMENT
+ON COLUMN log_operate.descr IS '操作描述';
+COMMENT
+ON COLUMN log_operate.status IS '操作状态:0-失败,1-成功';
+COMMENT
+ON COLUMN log_operate.error_msg IS '错误信息';
+COMMENT
+ON COLUMN log_operate.request_ip IS '请求IP';
+COMMENT
+ON COLUMN log_operate.request_url IS '请求的URL地址';
+COMMENT
+ON COLUMN log_operate.request_area IS '请求地区';
+COMMENT
+ON COLUMN log_operate.request_os IS '请求系统';
+COMMENT
+ON COLUMN log_operate.request_device IS '请求设备';
+COMMENT
+ON COLUMN log_operate.request_browser IS '请求浏览器';
+COMMENT
+ON COLUMN log_operate.method_name IS '被调用方法的全限定名（包名.类名.方法名）';
+COMMENT
+ON COLUMN log_operate.duration_ms IS '请求执行耗时:单位毫秒';
+COMMENT
+ON COLUMN log_operate.params IS '请求参数';
+COMMENT
+ON COLUMN log_operate.result IS '响应结果';
+COMMENT
+ON COLUMN log_operate.creator IS '创建人';
+COMMENT
+ON COLUMN log_operate.create_time IS '创建时间';
+COMMENT
+ON COLUMN log_operate.updater IS '更新人';
+COMMENT
+ON COLUMN log_operate.update_time IS '更新时间';
+COMMENT
+ON COLUMN log_operate.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
 
 
 -- ----------------------------
@@ -343,15 +463,27 @@ CREATE TABLE conf_region
 
 CREATE INDEX idx_cr_code ON conf_region (code);
 
-COMMENT ON TABLE conf_region IS '行政区划配置表';
-COMMENT ON COLUMN conf_region.id IS '行政区划ID';
-COMMENT ON COLUMN conf_region.pcode IS '父级代码';
-COMMENT ON COLUMN conf_region.code IS '行政区划代码';
-COMMENT ON COLUMN conf_region.name IS '行政区划名称';
-COMMENT ON COLUMN conf_region.level IS '层级:1-省;2-市;3-县（区）';
-COMMENT ON COLUMN conf_region.full_path IS '物化路径: /110000/110100/110101/';
-COMMENT ON COLUMN conf_region.creator IS '创建人';
-COMMENT ON COLUMN conf_region.create_time IS '创建时间';
-COMMENT ON COLUMN conf_region.updater IS '更新人';
-COMMENT ON COLUMN conf_region.update_time IS '更新时间';
-COMMENT ON COLUMN conf_region.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
+COMMENT
+ON TABLE conf_region IS '行政区划配置表';
+COMMENT
+ON COLUMN conf_region.id IS '行政区划ID';
+COMMENT
+ON COLUMN conf_region.pcode IS '父级代码';
+COMMENT
+ON COLUMN conf_region.code IS '行政区划代码';
+COMMENT
+ON COLUMN conf_region.name IS '行政区划名称';
+COMMENT
+ON COLUMN conf_region.level IS '层级:1-省;2-市;3-县（区）';
+COMMENT
+ON COLUMN conf_region.full_path IS '物化路径: /110000/110100/110101/';
+COMMENT
+ON COLUMN conf_region.creator IS '创建人';
+COMMENT
+ON COLUMN conf_region.create_time IS '创建时间';
+COMMENT
+ON COLUMN conf_region.updater IS '更新人';
+COMMENT
+ON COLUMN conf_region.update_time IS '更新时间';
+COMMENT
+ON COLUMN conf_region.del_flag IS '逻辑删除标识:0-未删除,1-已删除';
