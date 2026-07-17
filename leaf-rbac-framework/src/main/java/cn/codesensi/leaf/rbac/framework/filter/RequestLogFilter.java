59c96f9f7079a786ad4ce30242d085a3bb99b024
codesensi <codesensi@aliyun.com>
2026-07-17 19:51:38 +0800

    chore(filter): 移除请求日志过滤器的组件注解
    - 将 @Component 注解注释掉以禁用 RequestLogFilter 组件
    - 保留过滤器的基本结构和功能代码
    - 维持原有的执行顺序配置
    - 防止该过滤器在应用启动时被自动注册到Spring容器中

--- leaf-rbac-framework/src/main/java/cn/codesensi/leaf/rbac/framework/filter/RequestLogFilter.java
+++ leaf-rbac-framework/src/main/java/cn/codesensi/leaf/rbac/framework/filter/RequestLogFilter.java
@@ -26,7 +26,7 @@ import java.util.List;
  */
 @RequiredArgsConstructor
 @Slf4j
-@Component
+// @Component
 @Order(Ordered.HIGHEST_PRECEDENCE + 2)
 public class RequestLogFilter extends OncePerRequestFilter {
 
